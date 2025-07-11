0c5ef23f7ca196ddc2b4d460084b6e54ff31edce
Volkov Nikolay <volkovnd@yandex.ru>
2025-07-12 00:44:58 +0300

    feat: Added normal eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,59 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts'
+  ],
+  devtools: {
+    enabled: true, telemetry: false
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'shortcut icon',
+          type: 'image/x-icon',
+          href: `${import.meta.env.NUXT_APP_BASE_URL || '/'}favicon.ico`
+        }
+      ]
+    }
+  },
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true, telemetry: false},
-  telemetry:{enabled: false},
-  modules: ['@nuxt/eslint', '@nuxt/fonts']
+  telemetry: {
+    enabled: false
+  },
+
+  eslint: {
+    config: {
+      nuxt: {
+        sortConfigKeys: true
+      },
+      stylistic: {
+        semi: false,
+        arrowParens: true,
+        blockSpacing: true,
+        commaDangle: 'never',
+        indent: 2,
+        quotes: 'single'
+      }
+    }
+  },
+  fonts: {
+    families: [
+      {
+        name: 'Roboto',
+        display: 'swap',
+        provider: 'google',
+        weights: [
+          400,
+          500,
+          700
+        ],
+        subsets: [
+          'cyrillic',
+          'latin'
+        ]
+      }
+    ]
+  }
 })
