1218b5aaa8ff34203e9b5b7eb5a55a79fa57e193
Volkov Nikolay <volkovnd@yandex.ru>
2025-08-15 22:15:41 +0300

    fix: Replaced to nuxt.config.ts settings for app head tags and etc

--- app/app.vue
+++ app/app.vue
@@ -10,23 +10,6 @@
 useHead({
   titleTemplate(title) {
     return (title ? title + ' | ' : '') + 'Job search helper'
-  },
-  htmlAttrs: {
-    lang: 'ru',
-    translate: 'no'
-  },
-  meta: [
-    {
-      name: 'google',
-      content: 'notranslate'
-    }
-  ],
-  link: [
-    {
-      rel: 'shortcut icon',
-      type: 'image/x-icon',
-      href: `${import.meta.env.NUXT_APP_BASE_URL || '/'}favicon.ico`
-    }
-  ]
+  }
 })
 </script>
